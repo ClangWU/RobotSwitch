795f15acf420d2d2b3b0ef8366e1a43bf3643a72
ClangWu <clangwu@163.com>
2023-08-01 16:14:48 +0800

    [chg]multi node

--- src/robotswitch_controller/src/ahrs_driver.cpp
+++ src/robotswitch_controller/src/ahrs_driver.cpp
@@ -526,42 +526,6 @@ namespace RobotSwitch
         Euler_angles_pub_.publish(Euler_angles_2d);
         Magnetic_pub_.publish(Magnetic);
       }
-<<<<<<< HEAD
-
-      geometry_msgs::Twist fk_cartesian_msg;
-      fk_cartesian_msg.linear.x = 0;
-      fk_cartesian_msg.linear.y = 0;
-      fk_cartesian_msg.linear.z = 0;
-      fk_cartesian_msg.angular.x = imu_data.angular_velocity.x * 0.1f;
-      fk_cartesian_msg.angular.y = imu_data.angular_velocity.y * 0.1f;
-      fk_cartesian_msg.angular.z = imu_data.angular_velocity.z * 0.1f;
-      //clang
-      // double current_time_ms = ros::Time::now().toSec() * 1000;
-      // ROS_INFO("\033[1;31mCurrent time (ms): %.3f\033[0m", current_time_ms);
-      //5ms per time
-      imu_pub_.publish(imu_data);
-      velocity_command_publisher.publish(fk_cartesian_msg);
-}
-    //读取AHRS数据进行解析，并发布相关话题
-    else if (head_type[0] == TYPE_AHRS)
-    {
-      geometry_msgs::Pose2D pose_2d;
-      pose_2d.theta = ahrs_frame_.frame.data.data_pack.Heading;
-      mag_pose_pub_.publish(pose_2d);
-      //std::cout << "YAW: " << pose_2d.theta << std::endl;
-      geometry_msgs::Vector3 Euler_angles_2d,Magnetic;  
-      Euler_angles_2d.x = ahrs_frame_.frame.data.data_pack.Roll;
-      Euler_angles_2d.y = ahrs_frame_.frame.data.data_pack.Pitch;
-      Euler_angles_2d.z = ahrs_frame_.frame.data.data_pack.Heading;
-      Magnetic.x = imu_frame_.frame.data.data_pack.magnetometer_x;
-      Magnetic.y = imu_frame_.frame.data.data_pack.magnetometer_y;
-      Magnetic.z = imu_frame_.frame.data.data_pack.magnetometer_z;
-
-      Euler_angles_pub_.publish(Euler_angles_2d);
-      Magnetic_pub_.publish(Magnetic);
-=======
->>>>>>> one_node
-
       // 读取gps_pos数据进行解析，并发布相关话题
       else if (head_type[0] == TYPE_GEODETIC_POS)
       {
